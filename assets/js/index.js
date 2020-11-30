3647ed6ec189ca7f0aae948d4d47711acc4ad905
John <jgilpn@gmail.com>
2020-11-29 18:16:48 -0800

    updated GET/login to GET/user

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 if (window.localStorage.getItem('token')) {
-    fetch('https://mememaker-backend.herokuapp.com/login', {
+    fetch('https://mememaker-backend.herokuapp.com/user', {
         headers: {
             'authorization': 'JWT ' + window.localStorage.getItem('token')
         }
